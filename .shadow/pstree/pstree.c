94253d167d688b259e37a2f845be64fa52198e5c
tracer-nju <tracer@nju.edu.cn>
2023-03-15 16:55:50 +0800

    Linux chenyuheng-ubuntu 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:55:50 up  7:03,  2 users,  load average: 0.18, 0.58, 0.58

--- .shadow/pstree/pstree.c
+++ .shadow/pstree/pstree.c
@@ -31,7 +31,7 @@ void Print(pid_t pid, int depth){
   if(procs[pid]->cpid != NULL){
     int sz = procs[pid]->childnum;
     for(int i = 0;i < sz;++i){
-      Print(procs[pid]->cpid[i], depth + 2);
+      Print(procs[pid]->cpid[i], depth + 32);
       printf("\n");
       fflush(stdout);
     }  
