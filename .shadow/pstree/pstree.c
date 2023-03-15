4040f656feace62eb14737afc0e778abb62fb95b
tracer-nju <tracer@nju.edu.cn>
2023-03-15 16:10:27 +0800

    Linux chenyuheng-ubuntu 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:10:27 up  6:17,  3 users,  load average: 0.73, 0.62, 0.64

--- .shadow/pstree/pstree.c
+++ .shadow/pstree/pstree.c
@@ -161,7 +161,7 @@ void getproc(const char* dir){
               strncpy(procs[pid]->name, entry->d_name, 32);
               
               // read thread 
-              char procfile[256] = "";
+              char procfile[512] = "";
               sprintf(procfile, "/proc/%s", entry->d_name);
               getthread(procfile, pid);
               getchild(procfile, pid);
